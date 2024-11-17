a3ab212905d7a370a5b24df6384a79b68787c6f4
vladimirovodenko <vladimir@tallinn-learning.ee>
2024-11-17 20:12:24 +0200

    config changes viewport

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'data-name',
+    viewport: { width: 1920, height: 1080 },
   },
 
   /* Configure projects for major browsers */
